fb2bf9d0b0a65ac7e2cd9688a69b81141b7e6157
pkp <pkp@DESKTOP-Q9B3M99>
2025-05-18 14:30:04 +0530

    start

---
+++ src/demo21/firstline_of_project.java
@@ -0,0 +1,7 @@
+package demo21;
+
+public class firstline_of_project {
+  public static void main(String[] args) {
+	System.out.println("so are worried about me");
+  }
+}
---
+++ src/demo21/second_of_code.java
@@ -0,0 +1,8 @@
+package demo21;
+
+public class second_of_code {
+	public static void main(String[] args) {
+		System.out.println("Hey you are my friend");
+	}
+
+}
